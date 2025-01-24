74af95d263bb554839447718a90ede3d4d08edd1
Aspian <p.aspian1738@gmail.com>
2025-01-24 11:29:22 +0800

    web(008): tailwind, daisyui, flasher, siswa

--- sch/xi/Web/008/routes/web.php
+++ sch/xi/Web/008/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::resource("/", SiswaController::class);
