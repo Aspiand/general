6f071ea9119c032eaa7a854061415e046e281df9
Aspian <p.aspian1738@gmail.com>
2025-02-06 08:20:41 +0800

    java(005): base

---
+++ sch/Java/005/src/id/my/aspian/j005/Koneksi.java
@@ -0,0 +1,28 @@
+package id.my.aspian.j005;
+
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Koneksi {
+
+    private static Connection conn;
+
+    public static Connection koneksi() {
+        if (conn != null) {
+            return conn;
+        }
+
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/general",
+                    "root", "root"
+            );
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Gagal terkoneksi");
+        }
+
+        return conn;
+    }
+}
