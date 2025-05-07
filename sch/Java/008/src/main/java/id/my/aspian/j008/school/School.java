a6039335077f0312777630c45dd3fe80e554c6de
Aspian <muhammad.aspian.d@gmail.com>
2025-05-07 16:00:46 +0800

    java(008): school
    maven coy

---
+++ sch/Java/008/src/main/java/id/my/aspian/j008/school/School.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package id.my.aspian.j008.school;
+
+import id.my.aspian.j008.school.home.HomeView;
+import id.my.aspian.j008.school.utils.DBConnection;
+
+/**
+ *
+ * @author ao
+ */
+public class School {
+
+    public static void main(String[] args) {
+        HomeView hv = new HomeView();
+        hv.setVisible(true);
+    }
+}
---
+++ sch/Java/008/src/main/java/id/my/aspian/j008/school/utils/DBConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.my.aspian.j008.school.utils;
+
+import java.sql.*;
+
+/**
+ *
+ * @author ao
+ */
+public class DBConnection {
+
+    private static Connection connection = null;
+
+    public static synchronized Connection getDatabaseConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/j008",
+                        "root", "root");
+            } catch (SQLException e) {
+//                showErrorDialog("Can't connect to database");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        return connection;
+    }
+}
