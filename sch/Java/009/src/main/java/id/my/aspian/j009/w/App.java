2f29b1519587575e671c72e46259e62ade104a14
Aspian <muhammad.aspian.d@gmail.com>
2025-06-04 06:49:53 +0800

    java(009): simple weather app

---
+++ sch/Java/009/src/main/java/id/my/aspian/j009/w/App.java
@@ -0,0 +1,55 @@
+package id.my.aspian.j009.w;
+
+import java.net.URI;
+import java.net.http.*;
+import java.util.*;
+import com.google.gson.*;
+
+class WeatherResponse {
+
+    CurrentWeather current_weather;
+
+    static class CurrentWeather {
+
+        double temperature;
+        double windspeed;
+    }
+}
+
+public class App {
+
+    static Map<String, double[]> locations = Map.of(
+            "Jakarta", new double[]{-6.2, 106.8},
+            "Surabaya", new double[]{-7.25, 112.75},
+            "Bandung", new double[]{-6.91, 107.61}
+    );
+
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        Gson gson = new Gson();
+
+        for (var entry : locations.entrySet()) {
+            String city = entry.getKey();
+            Double latitude = entry.getValue()[0];
+            Double longitude = entry.getValue()[1];
+
+            String url = String.format("https://api.open-meteo.com/v1/forecast?latitude=%.4f&longitude=%.4f&current_weather=true", latitude, longitude);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                WeatherResponse weather = gson.fromJson(response.body(), WeatherResponse.class);
+                double temp = weather.current_weather.temperature;
+                double wind = weather.current_weather.windspeed;
+
+                System.out.printf("Cuaca di %s: %.1f°C, angin %.1f km/jam%n", city, temp, wind);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
